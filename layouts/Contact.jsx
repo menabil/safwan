b5852e36cb0ee6c00172045cdb39fd766827200a
menabil <nabil99cc@gmail.com>
2026-06-06 23:36:31 +0600

    responsive contact section

--- layouts/Contact.jsx
+++ layouts/Contact.jsx
@@ -5,18 +5,18 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <section className="py-30">
+    <section className="md:py-30 py-15">
       <Container>
-        <Flex className={"gap-x-7.5 justify-center"}>
-          <div className="w-117.5 h-146.5 bg-[#C4C4C4]"></div>
+        <Flex className={"gap-7.5 justify-center flex-col-reverse md:flex-row"}>
+          <div className="w-full md:w-117.5 h-146.5 bg-[#C4C4C4]"></div>
           <div className="">
             <p className="text-lg font-medium text-secondary font-Vol">
               Get In Touch
             </p>
-            <h3 className="text-[50px] font-medium leading-15 text-primary font-Vol mx-auto pt-3 pb-10">
+            <h3 className="text-3xl md:text-[50px] md:leading-15 font-medium text-primary font-Vol mx-auto pt-3 pb-10">
               Feel Free to Contact
             </h3>
-            <Flex className={"gap-x-15"}>
+            <Flex className={"gap-7 md:gap-x-15 flex-col md:flex-row"}>
               <input
                 type="text"
                 placeholder="Enter Name"
@@ -28,7 +28,7 @@ const Contact = () => {
                 className="outline-none pb-3 border-b-3 border-primary/10 placeholder:text-primary/70 placeholder:text-lg placeholder:font-Jos w-75"
               />
             </Flex>
-            <Flex className={"gap-x-15 py-14"}>
+            <Flex className={"gap-7 md:gap-x-15 flex-col md:flex-row py-14"}>
               <input
                 type="text"
                 placeholder="Enter Name"
