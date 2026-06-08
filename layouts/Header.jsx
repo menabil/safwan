fd0b79836fad0027d884fac092b7c57079c5056f
menabil <nabil99cc@gmail.com>
2026-06-08 23:35:12 +0600

    responsive header section

--- layouts/Header.jsx
+++ layouts/Header.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Container from "../components/Container";
 import Image from "next/image";
 import Flex from "@/components/Flex";
 import Button from "@/components/Button";
 import Logo from "../public/logo.png";
+import { HiMenuAlt1 } from "react-icons/hi";
 
 const Header = () => {
+  let [show, setShow] = useState(false);
+  let handelClick = () => {
+    setShow(!show);
+  };
   return (
     <nav className="py-5">
       <Container>
-        <Flex className={"justify-between font-Jos"}>
+        <Flex className={"justify-between font-Jos relative px-3 md:px-0"}>
           <Image
             src={Logo}
             alt="Logo"
@@ -17,7 +23,7 @@ const Header = () => {
             height={40}
             className="cursor-pointer"
           />
-          <ul className="flex items-center gap-x-10 text-primary text-lg">
+          <ul className="md:flex items-center gap-x-10 text-primary text-lg hidden">
             <li className="hover:text-secondary duration-300 cursor-pointer">
               Home
             </li>
@@ -34,7 +40,42 @@ const Header = () => {
               Contact
             </li>
           </ul>
-          <Button btnText={"Download CV"} className={"text-lg font-medium"} />
+
+          <p
+            onClick={handelClick}
+            className="block md:hidden text-3xl cursor-pointer text-[#192239]"
+          >
+            <HiMenuAlt1 />
+          </p>
+          {show && (
+            <div className="absolute top-full left-[5%] border-secondary border-4 w-[85%] bg-white p-5 rounded-2xl shadow-newMade md:hidden z-50">
+              <ul className="flex items-center text-primary text-lg flex-col gap-y-4">
+                <li className="hover:text-secondary duration-300 cursor-pointer">
+                  Home
+                </li>
+                <li className="hover:text-secondary duration-300 cursor-pointer">
+                  Pages
+                </li>
+                <li className="hover:text-secondary duration-300 cursor-pointer">
+                  Services
+                </li>
+                <li className="hover:text-secondary duration-300 cursor-pointer">
+                  Blog
+                </li>
+                <li className="hover:text-secondary duration-300 cursor-pointer">
+                  Contact
+                </li>
+              </ul>
+              <Button
+                btnText={"Download CV"}
+                className={"text-lg font-medium w-fit mx-auto mt-5"}
+              />
+            </div>
+          )}
+          <Button
+            btnText={"Download CV"}
+            className={"text-lg font-medium hidden md:block"}
+          />
         </Flex>
       </Container>
     </nav>
